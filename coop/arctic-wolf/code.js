ffe848acda548550c435a69e468dd8de4601c73d
Will Pringle <willkantorpringle+1@gmail.com>
2022-09-24 21:08:55 -0400

    add arctic wolf

---
+++ coop/arctic-wolf/code.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  let src = './Untitled.mp3';
+  let audio = new Audio(src);
+  audio.play();
+});
+
+
+//#dc8adc
