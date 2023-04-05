680cd680cb94d8f8c276ddd125547623c9b15cca
Will Pringle <willkantorpringle+1@gmail.com>
2023-04-04 20:13:56 -0400

    Publish new imgprobox website

--- imgprobox/_next/static/21S-HxMY3KixF4qfYbLTv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/665f4d0a2b8e4f15.css","static/chunks/pages/index-4a92543f21b945c9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
